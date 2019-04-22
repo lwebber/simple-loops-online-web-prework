da11fd248cd4a92fff88bba8bb26e4c8111cb5cc
Laura Webber <wwebby1@gmail.com>
2019-04-22 00:49:30 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -18,7 +18,7 @@ def loop_iterator(number_of_times)
   
 end
 
-def times_iterator(number_of_times)
+def times_iterator(number_of_rspetimes)
   # code your solution here using the "times" keyword
   phrase = "Welcome to Flatiron School's Web Development Course!"
   
