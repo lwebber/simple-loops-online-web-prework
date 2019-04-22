ed84f7b58ee212eca6d91095d4c18906d96c79fa
Laura Webber <wwebby1@gmail.com>
2019-04-22 01:12:05 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -31,6 +31,10 @@ def while_iterator(number_of_times)
   # code your solution here using the "while" keyword
   # hint: user a counter to tell the while loop when to stop!
   phrase = "Welcome to Flatiron School's Web Development Course!"
+  counter = number_of_times 
+  while counter >= 0 
+    puts phrase
+  end
   
 end
 
